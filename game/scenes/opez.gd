1c8b4e7dadcf952135ce0e7d3574bcff9c1c86dd
Tobias Schmidt <itoby99@proton.me>
2026-05-06 20:42:41 +0200

    made opez jumpscare

--- game/scenes/opez.gd
+++ game/scenes/opez.gd
@@ -1,15 +1,30 @@
 extends Node3D
 
-@onready var anim = $AnimationPlayer
+@onready var anim = $Visual/AnimationPlayer
 
+var player = null
+var is_talking := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	sit()
 
 func sit():
-	anim.play("sit")
+	anim.play("Sit")
+	
+func interact():
+	if is_talking:
+		return
+	print("Start Dialog...")
+	player = get_tree().get_first_node_in_group("player")
+	look_at_player()
+	# TODO start dialog
 
+func look_at_player():
+	var dir = (player.global_position - global_position).normalized()
+	var target_rot = atan2(dir.x, dir.z)
+	rotation.y = target_rot
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
